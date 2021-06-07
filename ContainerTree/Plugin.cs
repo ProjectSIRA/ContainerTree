f9c25909a084840cdf111d42ab4a272f98444148
Auros Nexus <aurosnex@gmail.com>
2021-06-06 21:43:21 -0400

    Initial commit

---
+++ ContainerTree/Plugin.cs
@@ -0,0 +1,28 @@
+﻿using IPA;
+using SiraUtil.Zenject;
+using IPALogger = IPA.Logging.Logger;
+
+namespace ContainerTree
+{
+    [Plugin(RuntimeOptions.DynamicInit)]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+
+        }
+    }
+}
